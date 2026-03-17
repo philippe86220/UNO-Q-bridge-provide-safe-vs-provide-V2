c27e3ca5180c166df7a7961553a848236f483d89
philippe86220 <142721115+philippe86220@users.noreply.github.com>
2026-03-17 09:37:39 +0100

    Add functions for printing to Monitor

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,1 +1,29 @@
+#include <Arduino.h>
+#include <Arduino_RouterBridge.h>
 
+void printSlow(const char *s) {
+  for (int i = 0; s[i] != '\0'; i++) {
+    Monitor.print(s[i]);
+    delay(10);
+  }
+  Monitor.println();
+}
+
+void printFromMpu(float value) {
+  Monitor.println(value, 2);
+}
+
+void setup() {
+  Bridge.begin();
+  Monitor.begin();
+
+  Bridge.provide("print_value", printFromMpu);
+  //Bridge.provide_safe("print_value", printFromMpu);
+
+  delay(5000);
+}
+
+void loop() {
+  printSlow("Forum");
+  delay(100);
+}
